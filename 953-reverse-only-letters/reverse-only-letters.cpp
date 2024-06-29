7438931b4341624aaa8a84ae440b089a1d5b389e
Kriti Omar <114827451+KritiOmar20@users.noreply.github.com>
2024-06-29 13:16:54 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (25.57%) - LeetSync

--- 953-reverse-only-letters/reverse-only-letters.cpp
+++ 953-reverse-only-letters/reverse-only-letters.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     string reverseOnlyLetters(string s) {
-       // int left = 0, right = s.size() - 1;
-       //  while (left < right) {
-       //      swap(s[left], s[right]);
-       //      left++;
-       //      right--;
-       //  }
-       //  return s;
+     
         int left = 0, right = s.length() - 1;
         while (left < right) {
         if (!isalpha(s[left])) {

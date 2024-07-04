139eceaffaee26880f5e3e3d46407cae30d56eaa
Kriti Omar <114827451+KritiOmar20@users.noreply.github.com>
2024-07-04 19:03:40 +0530

    Time: 78 ms (61.66%) | Memory: 63.7 MB (54.18%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        vector<bool> visited(n + 1, false);
+        for (int i = 0; i < n; ++i) {
+            if (visited[nums[i]]) {
+                return nums[i];
+            }
+            visited[nums[i]] = true;
+        }
+        return -1;
+        
+    }
+};

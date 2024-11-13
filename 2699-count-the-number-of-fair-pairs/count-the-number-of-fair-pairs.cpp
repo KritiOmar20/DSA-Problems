502c7e32cbdc86934a91f5e3bda3c707831cb5d9
Kriti Omar <114827451+KritiOmar20@users.noreply.github.com>
2024-11-13 18:29:32 +0530

    Time: 47 ms (80.36%) | Memory: 60.4 MB (61.66%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
@@ -1,13 +1,36 @@
 class Solution {
 public:
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
-        long long res = 0;
         sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size(); i++) {
-            int left = lower_bound(nums.begin() + i + 1, nums.end(), lower - nums[i]) - nums.begin();
-            int right = upper_bound(nums.begin() + i + 1, nums.end(), upper - nums[i]) - nums.begin();
-            res += right - left;
+        int n = nums.size();
+        long long count = 0;
+        for(int i = 0;i < n;i++){
+            int low = i + 1;
+            int high = n - 1;
+            int rem = lower - nums[i];
+            int ans = n;
+            while(low <= high){
+                int mid = (low + high) / 2;
+                if(nums[mid] >= rem){
+                    ans = mid;
+                    high = mid - 1;
+                }
+                else low = mid + 1;
+            }
+            low = i + 1;
+            high = n - 1;
+            rem = upper - nums[i];
+            int cnt = n;
+            while(low <= high){
+                int mid = (low + high) / 2;
+                if(nums[mid] > rem){
+                    cnt = mid;
+                    high = mid - 1;
+                }
+                else low = mid + 1;
+            }
+            count += (cnt - ans);
         }
-        return res;
+        return count;
     }
 };

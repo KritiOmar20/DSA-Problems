b7d9b4cec0413e8b532beee439b65d71beb58dc1
Kriti Omar <114827451+KritiOmar20@users.noreply.github.com>
2025-02-17 14:52:41 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (65.95%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -4,13 +4,13 @@ public:
         int low=0, high = nums.size()-1;
         while(low<=high){
             int mid = low+(high-low)/2;
-            if(nums[mid] > nums[high]){
+            if(nums[mid] > nums[high]){ // minimum element must be on the right side of mid
                 low = mid + 1;
             }
-            else if(nums[mid] < nums[high]){
+            else if(nums[mid] < nums[high]){ //minimum element is at mid or on the left side.
                 high =mid;
             }
-            else{
+            else{ // handle duplicate
                 high--;
             }
         }

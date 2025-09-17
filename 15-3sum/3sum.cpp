e49ff935c13c9b1ac3289a63c3aa0ffdc0a1d0ca
Kriti Omar <114827451+KritiOmar20@users.noreply.github.com>
2025-09-17 11:21:21 +0530

    Time: 1176 ms (10.30%) | Memory: 293.6 MB (8.41%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -2,30 +2,32 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> result;
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
+        if (nums.size() < 3) return result;
 
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+        sort(nums.begin(), nums.end());
+        set<vector<int>> uniqueTriplets;
+
+        for (int i = 0; i < nums.size() - 2; i++) {
             int left = i + 1;
-            int right = n - 1;
+            int right = nums.size() - 1;
+
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
-                if (sum == 0) {
-                    result.push_back({nums[i], nums[left], nums[right]});
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
 
+                if (sum == 0) {
+                    uniqueTriplets.insert({nums[i], nums[left], nums[right]});
                     left++;
                     right--;
-                }
-                else if (sum < 0) {
+                } else if (sum < 0) {
                     left++;
-                }
-                else {
+                } else {
                     right--;
                 }
             }
+        }
+
+        for (auto &triplet : uniqueTriplets) {
+            result.push_back(triplet);
         }
 
         return result;

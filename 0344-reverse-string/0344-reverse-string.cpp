871f3117350c35a3a5631807d11892a99d16422e
Kriti Omar <114827451+KritiOmar20@users.noreply.github.com>
2026-06-24 13:41:11 +0530

    Time: 0 ms (100%), Space: 27.1 MB (80.48%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        helper(s, 0, s.size() - 1);
+    }
+    void helper(vector<char>& s, int start, int end) {
+        if(start >= end) return; 
+        swap(s[start], s[end]);           
+        helper(s, start + 1, end - 1);    
+    }
+};

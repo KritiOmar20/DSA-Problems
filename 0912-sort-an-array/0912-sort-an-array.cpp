e01dbcfbefbe794494c8e018e6edf772195ebc5f
Kriti Omar <114827451+KritiOmar20@users.noreply.github.com>
2026-06-30 15:40:49 +0530

    Time: 355 ms (54.39%), Space: 183.9 MB (45.97%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
+    void merge(vector<int>& nums, int l, int m, int r) {
+        vector<int> left(nums.begin() + l, nums.begin() + m + 1);
+        vector<int> right(nums.begin() + m + 1, nums.begin() + r + 1);
+
+        int i = 0, j = 0, k = l;
+        while (i < left.size() && j < right.size()) {
+            if (left[i] <= right[j])
+                nums[k++] = left[i++];
+            else
+                nums[k++] = right[j++];
+        }
+
+        while (i < left.size())
+            nums[k++] = left[i++];
+
+        while (j < right.size())
+            nums[k++] = right[j++];
+    }
+
+    void mergeSort(vector<int>& nums, int l, int r) {
+        if (l >= r) return;
+
+        int m = l + (r - l) / 2;
+        mergeSort(nums, l, m);
+        mergeSort(nums, m + 1, r);
+        merge(nums, l, m, r);
+    }
+
     vector<int> sortArray(vector<int>& nums) {
-        int m = nums[0];
-        int M = nums[0];
-        for (int i = 0; i < nums.size(); i++) {
-            m = min(m, nums[i]);
-            M = max(M, nums[i]);
-        }
-        if (m == M) {
-            return nums;
-        }
-        // filling count array
-        int range = M - m + 1;
-        vector<int> count(range);
-        for (int i = 0; i < nums.size(); i++) {
-            count[nums[i] - m]++;
-        }
-        // now rewriting nums
-        int k = 0;
-        for (int i = 0; i < range; i++) {
-            while (count[i]-- > 0) {
-                nums[k++] = i + m;
-            }
-        }
+        mergeSort(nums, 0, nums.size() - 1);
         return nums;
     }
 };

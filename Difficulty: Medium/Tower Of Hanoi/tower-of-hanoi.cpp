43d56ac95a37fb942fbc143b0058fb6ab457129a
Kriti Omar <114827451+KritiOmar20@users.noreply.github.com>
2026-06-30 15:16:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Tower Of Hanoi/tower-of-hanoi.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    int towerOfHanoi(int n, int from, int to, int aux) {
+        if (n == 0)
+            return 0;
+        int left = towerOfHanoi(n - 1, from, aux, to);
+        int right = towerOfHanoi(n - 1, aux, to, from);
+        return left + 1 + right;
+    }
+};

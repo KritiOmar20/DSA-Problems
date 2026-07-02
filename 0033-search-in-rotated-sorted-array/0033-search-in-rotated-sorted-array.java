7b41a600763a64f92455d1273ce22755f856f8b7
Kriti Omar <114827451+KritiOmar20@users.noreply.github.com>
2026-07-02 11:10:42 +0530

    Time: 0 ms (100%), Space: 43.9 MB (46.37%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -8,13 +8,13 @@ class Solution {
                 return mid;
             }
             if(nums[left]<=nums[mid]){
-                if(nums[left]<=target && target<=nums[mid]){
+                if(nums[left]<=target && target<nums[mid]){
                     right=mid-1;
                 }
                 else left=mid+1;
             }
             else{
-                if(nums[mid+1]<=target && target<=nums[right]){
+                if(nums[mid]<target && target<=nums[right]){
                     left=mid+1;
                 }
                 else right=mid-1;

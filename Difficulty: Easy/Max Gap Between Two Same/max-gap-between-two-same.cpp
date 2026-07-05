2ebe4c5066397dbf5e5e629bff2699548b84d5ae
Kriti Omar <114827451+KritiOmar20@users.noreply.github.com>
2026-07-05 12:46:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Max Gap Between Two Same/max-gap-between-two-same.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    virtual int maxCharGap(string &s){
+      int st[26];
+      int ed[26];
+      
+      memset(st, -1, sizeof(st));
+      memset(ed, -1, sizeof(ed));
+      
+      int n=s.size();
+      for(int i=0; i<n; i++){
+        char ch=s[i];
+        if(st[(ch-97)] == -1) st[(ch-97)]=i;  
+      }     
+      
+      for(int i=n-1; i>=0; i--){
+        char ch=s[i];  
+        if(ed[(ch-97)] == -1) ed[(ch-97)]=i;  
+      }
+      
+      
+      
+      int ans=-1;
+      for(int i=0; i<n; i++){
+        char ch=s[i];  
+        ans=max(ans, ed[(ch-97)]-st[(ch-97)]-1);  
+      }
+      
+      return ans;
+    }
+};

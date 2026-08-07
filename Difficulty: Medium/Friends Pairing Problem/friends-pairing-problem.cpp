668661f072beeec35eb289d8536fe40c40975a2c
Kriti Omar <114827451+KritiOmar20@users.noreply.github.com>
2026-08-07 18:36:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Friends Pairing Problem/friends-pairing-problem.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int countFriendsPairings(int n) {
+        // code here
+        vector<long long> dp(n + 1, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + 1LL * (i - 1) * dp[i - 2];
+        }
+        return dp[n];
+    }
+};
